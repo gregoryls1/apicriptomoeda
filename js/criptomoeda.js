82e1660918501154b6ae1ac368bbcca77c355d5a
gregoryls1 <contatogregory@gmail.com>
2018-08-05 11:48:58 -0300

    correção

--- js/criptomoeda.js
+++ js/criptomoeda.js
@@ -1,10 +1,20 @@
-function reqListener () {
-  var moedas = JSON.parse(this.responseText);  
-  var table;
-  var opcoes;
-  
-  moedas.forEach(function(moeda){
-      table += `
+var apiBitcoin = function() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(xhr.response);
+            doTable(response);
+        }
+    }
+    xhr.open("GET", "https://api.coinmarketcap.com/v1/ticker/?convert=brl&limit=10", true);
+    xhr.send();
+}
+
+var doTable = function(resposta) {
+    var table = "";
+    resposta.forEach(function(moeda) {
+        table+= `
             <tr>
                 <th scope="row">${moeda.rank}</th>
                 <td>${moeda.name}</td>
@@ -14,13 +24,33 @@ function reqListener () {
             </tr>
             
             `
-     return document.getElementById("tabela").innerHTML = table;
-  })
-};
+    });
 
+    var tabela = document.getElementById("tabela");
+    tabela.innerHTML = table;
+}
 
-var oReq = new XMLHttpRequest();
-oReq.onload = reqListener;
-oReq.open("get", "https://api.coinmarketcap.com/v1/ticker/?convert=brl&limit=10", true);
-oReq.send();
+apiBitcoin();
 
+var acharMoeda = function() {
+    var xhr2 = new XMLHttpRequest();
+    var valor = document.getElementById("opcoes").value;
+    xhr2.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var resposta = JSON.parse(xhr2.response);
+            resposta = parseFloat(resposta[0].price_brl);
+            calcValor(resposta);
+        }
+    }
+    xhr2.open("GET", "https://api.coinmarketcap.com/v1/ticker/" + valor + "/?convert=brl", true);
+    xhr2.send();
+}
+
+var calcValor = function(valor) {
+    var convertido = 0.0;
+    var valorDivisao = document.getElementById("valor-converter").value;
+    var valorSelecionado = document.getElementById("opcoes").value;
+
+    convertido = valor / parseFloat(valorDivisao);
+    document.getElementById("resultado").textContent = convertido.toFixed(4) + " " + valorSelecionado;
+}
